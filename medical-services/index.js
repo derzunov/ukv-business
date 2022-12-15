1ea99180dec57d9c1e1089084914ca46e1337c47
derzunov <derzunov.work@gmail.com>
2022-12-15 22:56:39 +0300

    Работа с хэшами урла для внешних ссылок, скелет документов (надо рисовать их js-ом, иначе html будет нечитаем), мелкие правки

--- medical-services/index.js
+++ medical-services/index.js
@@ -5,13 +5,15 @@ const licensingButton_2 = document.getElementById("ms-licensing-button-2");
 const licensingBlock_1 = document.getElementById("ms-licensing-info-1");
 const licensingBlock_2 = document.getElementById("ms-licensing-info-2");
 
+// Block for scroll into view (Licensing)
+const licensingCommonBlock = document.getElementById("ms-licensing");
+
 // Class constants
 const ACTIVE_LICENSING_BUTTON_CLASS = "ms-licensing-button_active"
 const HIDDEN_CLASS = "ms-g-hidden"
 
-// Code :)
-licensingButton_1.addEventListener("click", () => {
-
+// Functions
+const showPreLicensing = () => {
     if ( !licensingButton_1.classList.contains(ACTIVE_LICENSING_BUTTON_CLASS) ) {
         licensingButton_2.classList.remove(ACTIVE_LICENSING_BUTTON_CLASS);
         licensingButton_1.classList.add(ACTIVE_LICENSING_BUTTON_CLASS);
@@ -19,10 +21,8 @@ licensingButton_1.addEventListener("click", () => {
         licensingBlock_1.classList.remove(HIDDEN_CLASS);
         licensingBlock_2.classList.add(HIDDEN_CLASS);
     }
-
-});
-
-licensingButton_2.addEventListener("click", () => {
+}
+const showPostLicensing = () => {
     if ( !licensingButton_2.classList.contains(ACTIVE_LICENSING_BUTTON_CLASS) ) {
         licensingButton_1.classList.remove(ACTIVE_LICENSING_BUTTON_CLASS);
         licensingButton_2.classList.add(ACTIVE_LICENSING_BUTTON_CLASS);
@@ -30,4 +30,25 @@ licensingButton_2.addEventListener("click", () => {
         licensingBlock_2.classList.remove(HIDDEN_CLASS);
         licensingBlock_1.classList.add(HIDDEN_CLASS);
     }
+}
+
+// Code :)
+// Check up hash for external links
+if(window.location.hash && window.location.hash === "#prelicensing") {
+    showPreLicensing();
+    licensingCommonBlock.scrollIntoView();
+} else if ( window.location.hash && window.location.hash === "#postlicensing") {
+    showPostLicensing();
+    licensingCommonBlock.scrollIntoView();
+}
+
+// Buttons listeners
+licensingButton_1.addEventListener("click", () => {
+    showPreLicensing();
+    window.location.hash = "#prelicensing";
+});
+
+licensingButton_2.addEventListener("click", () => {
+    showPostLicensing();
+    window.location.hash = "#postlicensing";
 });
